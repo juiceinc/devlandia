3cbb6a849a97013ea7b337ff4464e1d18b88d152
Casey Wireman <cwire4@gmail.com>
2017-06-07 19:04:44 -0400

    Updating setup script, added 2 new run configs to use for debug fix

---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+pip install virtualenv virtualenvwrapper
+echo "WORKON_HOME="~/.virtualenvs"" >> ~/.bash_profile
+echo "source /usr/local/bin/setup.sh" >> ~/.bash_profile
+source $HOME/.bash_profile
+chmod 400 insecure_key && cp insecure_key ~/.ssh/
+mkvirtualenv devlandia -p /usr/local/Cellar/python/2.7.13/bin/python
+echo "cd $PWD" >> ~/.virtualenvs/devlandia/bin/postactivate
+source $HOME/.virtualenvs/devlandia/bin/activate
+pip install -r requirements.txt
+sudo mkdir /code
+sudo touch /code/manage.py
--- scripts/virtualenvwrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pip install virtualenvwrapper
-echo "WORKON_HOME="~/.virtualenvs"" >> ~/.bash_profile
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bash_profile
-source $HOME/.bash_profile
-mkvirtualenv devlandia -p /usr/bin/python
