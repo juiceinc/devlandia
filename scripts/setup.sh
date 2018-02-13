f4e1dc00ec2d6b051e4a85bf3f278f8f39a3430f
Casey Wireman <cwire4@gmail.com>
2018-02-13 11:28:38 -0500

    Update setup.sh
    Updates virtualenv to use 2.7.14 since that's the version brew install python installs now

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ echo "WORKON_HOME="~/.virtualenvs"" >> ~/.bash_profile
 echo "source /usr/local/bin/setup.sh" >> ~/.bash_profile
 source $HOME/.bash_profile
 chmod 400 insecure_key && cp insecure_key ~/.ssh/
-mkvirtualenv devlandia -p /usr/local/Cellar/python/2.7.13/bin/python
+mkvirtualenv devlandia -p /usr/local/Cellar/python/2.7.14/bin/python
 echo "cd $PWD" >> ~/.virtualenvs/devlandia/bin/postactivate
 source $HOME/.virtualenvs/devlandia/bin/activate
 pip install -r requirements.txt
