05e91d5d58c6d1359844bf07271349214a788fbb
Michael F. Lamb <mike.lamb@juiceanalytics.com>
2018-06-21 12:27:12 -0700

    restore apps/__init__.py
    when we truncated the history for this repo, this file was not correctly
    added back in, since /apps is listed in .gitignore.

---
+++ apps/__init__.py
@@ -0,0 +1,11 @@
+import importlib
+import os
+
+current_directory = os.path.dirname(os.path.realpath(__file__))
+
+list_of_apps = [x for x in os.listdir(current_directory)
+                if os.path.isdir(os.path.join(current_directory, x)) and not x.startswith('.')]
+
+for app in list_of_apps:
+    if 'builds' not in app:
+        importlib.import_module('apps.{0}'.format(app))
