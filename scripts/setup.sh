4581d919efe3daa56d69c799a94672cb0e89588c
Michael F. Lamb <mike.lamb@juiceanalytics.com>
2018-06-21 14:46:52 -0700

    remove ugly bash tracing, add a bunch of echo statements.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,17 +6,40 @@ if [ ! -e insecure_key ]; then
     exit 1
 fi
 
-set -x
-
+echo "installing virtualenv and virtualenvwrapper..."
 pip install virtualenv virtualenvwrapper
 
+echo "modifying ~/.bash_profile if needed..."
 grep "WORKON_HOME" ~/.bash_profile || echo "WORKON_HOME="~/.virtualenvs"" >> ~/.bash_profile
 grep "/usr/local/bin/virtualenvwrapper.sh" ~/.bash_profile || echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bash_profile
+
+echo "sourcing ~/.bash_profile..."
 source $HOME/.bash_profile
-chmod 400 insecure_key && cp insecure_key ~/.ssh/
+
+echo "copying insecure ssh key into place if needed..."
+if [ ! -e ~/.ssh/insecure_key ]; then
+    chmod 400 insecure_key
+    cp insecure_key ~/.ssh/
+fi
+
+echo "creating virtualenv \"devlandia\"..."
 mkvirtualenv devlandia -p python2.7
 echo "cd $PWD" >> ~/.virtualenvs/devlandia/bin/postactivate
+
+echo "activating virtualenv \"devlandia\""
 source $HOME/.virtualenvs/devlandia/bin/activate
+
+echo "installing requirements into \"devlandia\"..."
 pip install -r requirements.txt
-sudo mkdir /code
-sudo touch /code/manage.py
+
+echo "creating /code/manage.py if needed..."
+if [ ! -e /code/manage.py ]; then
+   sudo mkdir /code
+   sudo touch /code/manage.py
+fi
+
+echo "Setup complete. If you saw no catastrophic errors above,"
+echo "then you should be able to **open a new terminal** and run"
+echo
+echo "    workon devlandia"
+echo
