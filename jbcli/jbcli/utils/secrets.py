355fdbd4e427d6b5bb6e0069ed6409d2c413d41d
Christopher Armstrong <227068+radix@users.noreply.github.com>
2019-08-20 17:40:56 -0500

    delay constructing the client to when get_paramstore is called

--- jbcli/jbcli/utils/secrets.py
+++ jbcli/jbcli/utils/secrets.py
@@ -2,9 +2,8 @@ from __future__ import print_function
 
 import boto3
 
-ssm = boto3.client('ssm')
-
 
 def get_paramstore(param_name):
+    ssm = boto3.client('ssm')
     parameter = ssm.get_parameter(Name=param_name, WithDecryption=True)
     return parameter['Parameter']['Value'].encode('ascii')
