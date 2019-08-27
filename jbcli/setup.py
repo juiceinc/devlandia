548f22718f7e30bc5c6cf209b912f4f7c4528371
Christopher Armstrong <227068+radix@users.noreply.github.com>
2019-08-27 11:22:19 -0500

    bump version from 0.11.0 to 0.11.1

--- jbcli/setup.py
+++ jbcli/setup.py
@@ -24,7 +24,7 @@ jb-manage=jbcli.cli.jb_manage:cli
 
 setup(
     name='jbcli',
-    version='0.11.0',
+    version='0.11.1',
     description='Juicebox command line',
     author="Casey Wireman",
     author_email='casey.wireman@juiceanalytics.com',
