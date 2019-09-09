969769d65d0ebdbac5fb7c4f95eb2dd422da44d5
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2019-09-09 10:48:50 -0500

    Fix tests

---
+++ jbcli/jbcli/tests/test_jbapiutil.py
@@ -0,0 +1,66 @@
+from mock import call, patch
+import requests_mock
+
+from ..utils import jbapiutil
+
+
+class TestAPIUtil:
+    def test_get_admin_token(self):
+        """Test a successful login. """
+        with requests_mock.Mocker() as m:
+            url = "{SERVER}/api/v1/jb/api-token-auth/".format(SERVER=jbapiutil.SERVER)
+            m.post(url, json={"token": "foo"})
+
+            val = jbapiutil.get_admin_token()
+            assert val == "foo"
+
+    def test_get_admin_token_failed(self):
+        """Test a bad login. """
+        with requests_mock.Mocker() as m:
+            url = "{SERVER}/api/v1/jb/api-token-auth/".format(SERVER=jbapiutil.SERVER)
+            m.post(url, status_code=400)
+
+            val = jbapiutil.get_admin_token()
+            assert val is None
+
+    @patch("jbcli.utils.jbapiutil.get_admin_token")
+    def test_load_app_notoken(self, mock_admin_token):
+        """Test app load when no token returned. """
+        mock_admin_token.return_value = None
+
+        with requests_mock.Mocker() as m:
+            url = "{SERVER}/api/v1/app/load/{APP}/".format(
+                SERVER=jbapiutil.SERVER, APP="meow"
+            )
+            m.post(url, status_code=200, json={"hi": "there"})
+
+            val = jbapiutil.load_app("meow")
+            assert val is False
+
+    @patch("jbcli.utils.jbapiutil.get_admin_token")
+    def test_load_app_success(self, mock_admin_token):
+        """Test a successful app load. """
+        mock_admin_token.return_value = "foo"
+
+        with requests_mock.Mocker() as m:
+            url = "{SERVER}/api/v1/app/load/{APP}/".format(
+                SERVER=jbapiutil.SERVER, APP="meow"
+            )
+            m.post(url, status_code=200, json={"hi": "there"})
+
+            val = jbapiutil.load_app("meow")
+            assert val is True
+
+    @patch("jbcli.utils.jbapiutil.get_admin_token")
+    def test_load_app_failure(self, mock_admin_token):
+        """Test a failed app load. """
+        mock_admin_token.return_value = "foo"
+
+        with requests_mock.Mocker() as m:
+            url = "{SERVER}/api/v1/app/load/{APP}/".format(
+                SERVER=jbapiutil.SERVER, APP="meow"
+            )
+            m.post(url, status_code=400, json={"no": "good"})
+
+            val = jbapiutil.load_app("meow")
+            assert val is False
--- jbcli/jbcli/utils/jbapiutil.py
+++ jbcli/jbcli/utils/jbapiutil.py
@@ -1,25 +1,25 @@
-import requests
+from requests import post
 import os
 from .format import *
+from pprint import pformat
 
-SERVER = 'http://localhost:8000'
-JB_ADMIN_USER = os.environ.get('JB_ADMIN_USER', 'chris@juice.com')
-JB_ADMIN_PASSWORD = os.environ.get('JB_ADMIN_PASSWORD', 'cremacuban0!')
+SERVER = "http://localhost:8000"
+JB_ADMIN_USER = os.environ.get("JB_ADMIN_USER", "chris@juice.com")
+JB_ADMIN_PASSWORD = os.environ.get("JB_ADMIN_PASSWORD", "cremacuban0!")
 
 
 def get_admin_token():
     """Get an admin user token. """
-    url = '{SERVER}/api/v1/jb/api-token-auth/'.format(SERVER=SERVER)
-    payload = {
-        'email': JB_ADMIN_USER,
-        'password': JB_ADMIN_PASSWORD,
-    }
-    response = requests.post(url, data=payload)
+    url = "{SERVER}/api/v1/jb/api-token-auth/".format(SERVER=SERVER)
+    payload = {"email": JB_ADMIN_USER, "password": JB_ADMIN_PASSWORD}
+    response = post(url, data=payload)
     if response.status_code == 200:
-        echo_success('Admin token acquired')
-        return response.json()['token']
+        echo_success("Admin token acquired")
+        return response.json()["token"]
     else:
-        echo_warning('Could not fetch admin token, status {}'.format(response.status_code))
+        echo_warning(
+            "Could not fetch admin token, status {}".format(response.status_code)
+        )
         return None
 
 
@@ -27,22 +27,24 @@ def load_app(app):
     """Attempt to load an app using jb API. If successful return True. """
     admin_token = get_admin_token()
     if admin_token:
-        url = '{SERVER}/api/v1/app/load/{APP}/'.format(SERVER=SERVER, APP=app)
+        url = "{SERVER}/api/v1/app/load/{APP}/".format(SERVER=SERVER, APP=app)
         headers = {
-            'Authorization': 'JWT {}'.format(admin_token),
-            'Content-Type': 'application-json'
+            "Authorization": "JWT {}".format(admin_token),
+            "Content-Type": "application-json",
         }
-        response = requests.post(url, headers=headers)
+        response = post(url, headers=headers)
         if response.status_code == 200:
             result = response.json()
-            echo_success('{} was added successfully via API.'.format(app))
+            echo_success("{} was added successfully via API.".format(app))
             # TODO: We could format the logs nicely
-            echo_success(result)
+            echo_success(pformat(result))
             return True
         else:
-            echo_warning('Loading app status code was {}'.format(response.status_code))
+            result = response.json()
+            echo_warning("Loading app status code was {}".format(response.status_code))
             # TODO: We could format the logs nicely
-            echo_warning(response.json())
+            echo_warning(pformat(result))
             return False
     else:
+        echo_warning("Could not get admin token.")
         return False
