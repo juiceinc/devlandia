7c593d4452d8d9b86fd169d473c28b5fbe35bd20
Christopher Armstrong <227068+radix@users.noreply.github.com>
2019-09-23 18:35:44 -0500

    use fruition.settings.contained for the django settings module of devlandia
    also some tweaks with the way secrets are named / stored

--- jbcli/jbcli/utils/secrets.py
+++ jbcli/jbcli/utils/secrets.py
@@ -1,9 +1,16 @@
 from __future__ import print_function
 
 import boto3
+import six
 
 
-def get_paramstore(param_name):
+def get_deployment_secrets():
     ssm = boto3.client('ssm')
-    parameter = ssm.get_parameter(Name=param_name, WithDecryption=True)
-    return parameter['Parameter']['Value'].encode('ascii')
+    result = ssm.get_parameters_by_path(
+        Path='/jb-deployment-vars/',
+        Recursive=True, WithDecryption=True,
+    )
+    # These ENCODE calls need to change for python 3 support
+    env = {param['Name'][len('/jb-deployment-vars/'):].encode('ascii'): param['Value'].encode('ascii') for param in result['Parameters']}
+    return env
+
