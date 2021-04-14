4987daf98b49e89ab703ac7df54985db90a92519
David Paul <david.paul@juiceanalytics.com>
2021-04-14 15:15:48 -0500

    remove commented line

--- jbcli/jbcli/utils/subprocess.py
+++ jbcli/jbcli/utils/subprocess.py
@@ -22,8 +22,6 @@ def check_call(args, env=None):
         echo_warning(f"Error:\n      {e}\n")
         sys.exit(1)
 
-    # return subprocess.check_call(args, env=env)
-
 def check_output(args, env=None):
     if win32api is not None:
         args[0] = win32api.FindExecutable(args[0])[1]
