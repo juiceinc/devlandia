1e4bd2af653ddad4febb1691b96869cb20b84e1e
David Paul <david.paul@juiceanalytics.com>
2021-04-14 13:29:11 -0500

    removed traceback from check_call

--- jbcli/jbcli/utils/subprocess.py
+++ jbcli/jbcli/utils/subprocess.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 
 import subprocess
 from subprocess import CalledProcessError
+from .format import echo_warning
+import sys
 
 __all__ = ['CalledProcessError', 'check_call', 'check_output']
 
@@ -14,7 +16,13 @@ except:
 def check_call(args, env=None):
     if win32api is not None:
         args[0] = win32api.FindExecutable(args[0])[1]
-    return subprocess.check_call(args, env=env)
+    try:
+        subprocess.check_call(args, env=env)
+    except subprocess.CalledProcessError as e:
+        echo_warning(f"Error:\n      {e}\n")
+        sys.exit(1)
+
+    # return subprocess.check_call(args, env=env)
 
 def check_output(args, env=None):
     if win32api is not None:
