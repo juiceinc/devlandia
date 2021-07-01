6dc7e70c931ce0b4dd976b4ec0c340b83c2b1cb1
Casey Wireman <cwire4@gmail.com>
2021-07-01 16:13:10 -0400

    allow users to select which mfa they should authenticate with if they have more than one

--- jbcli/jbcli/utils/auth.py
+++ jbcli/jbcli/utils/auth.py
@@ -9,16 +9,23 @@ def set_creds():
         try:
             with open(f"{os.path.expanduser('~')}/.aws/config") as config:
                 echo_success("Found ~/.aws/config file.")
-                for line in config:
-                    if "mfa_serial" in line:
-                        mfa_serial = line.split("=")[1].rstrip()
-                        break
+                mfas = [line.split("=")[1].rstrip() for line in config if "mfa_serial" in line]
+                deduped_mfas = list(dict.fromkeys(mfas))
+                echo_success(f"{deduped_mfas}")
+                if len(deduped_mfas) < 1:
+                    echo_warning("Error: No MFA Devices Found in Config")
+                    exit(1)
+                if len(deduped_mfas) == 1:
+                    choice = 0
+                elif len(deduped_mfas) > 1:
+                    choice = int(input(f"Select index of MFA Serial to use (0-{len(deduped_mfas)-1}): "))
+
         except FileNotFoundError:
             echo_warning('~/.aws/config file not found.')
-        if mfa_serial is not None:
+        if deduped_mfas[choice] is not None:
             token = input("Please enter MFA Code: ")
             output = json.loads(
-                subprocess.check_output(['aws', 'sts', 'get-session-token', '--serial-number', f'{mfa_serial}',
+                check_output(['aws', 'sts', 'get-session-token', '--serial-number', f'{deduped_mfas[choice]}',
                                          '--token-code', f'{token}']))
             os.environ['AWS_ACCESS_KEY_ID'] = output['Credentials']['AccessKeyId']
             os.environ['AWS_SECRET_ACCESS_KEY'] = output['Credentials']['SecretAccessKey']
