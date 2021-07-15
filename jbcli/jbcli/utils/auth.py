1f51b3ef856e0ae9c7b837d7f8fd5810f2d412f6
Casey Wireman <cwire4@gmail.com>
2021-07-15 15:34:34 -0400

    test

--- jbcli/jbcli/utils/auth.py
+++ jbcli/jbcli/utils/auth.py
@@ -3,31 +3,43 @@ from .subprocess import check_call, check_output
 from .format import echo_warning, echo_success
 import json
 
+
 def set_creds():
     if "AWS_ACCESS_KEY_ID" not in os.environ:
         mfa_serial = None
+        mfas = []
+        profiles = []
         choice = 0
         try:
             with open(f"{os.path.expanduser('~')}/.aws/config") as config:
                 echo_success("Found ~/.aws/config file.")
-                mfas = [line.split("=")[1].rstrip() for line in config if "mfa_serial" in line]
+                for line in config:
+                    if '[' in line and 'profile' not in line:
+                        profiles.append(line.replace('[', '').replace(']', '').rstrip())
+                    elif '[' in line and 'profile' in line:
+                        profiles.append(line.replace('[', '').replace(']', '').replace('profile ', '').rstrip())
+                    elif 'mfa_serial' in line:
+                        mfas.append(line.split('=')[1].rstrip())
+                echo_success(f'Profiles: {profiles}')
                 deduped_mfas = list(dict.fromkeys(mfas))
-                echo_success(f"{deduped_mfas}")
+                echo_success(f"MFAs: {deduped_mfas}")
                 if len(deduped_mfas) < 1:
                     echo_warning("Error: No MFA Devices Found in Config")
                     exit(1)
                 if len(deduped_mfas) == 1:
                     choice = 0
                 elif len(deduped_mfas) > 1:
-                    choice = int(input(f"Select index of MFA Serial to use (0-{len(deduped_mfas)-1}): "))
+                    choice = int(input(f"Select index of MFA Serial to use (0-{len(deduped_mfas) - 1}): "))
 
         except FileNotFoundError:
             echo_warning('~/.aws/config file not found.')
         if deduped_mfas[choice] is not None:
             token = input("Please enter MFA Code: ")
+            profile_name = input("Please enter profile name: ")
             output = json.loads(
-                check_output(['aws', 'sts', 'get-session-token', '--serial-number', f'{deduped_mfas[choice]}',
-                                         '--token-code', f'{token}']))
+                check_output(['aws', 'sts', 'get-session-token', '--profile', f'{profile_name}', '--serial-number', f'{deduped_mfas[choice]}',
+                              '--token-code', f'{token}', '--duration-seconds', '86400']))
             os.environ['AWS_ACCESS_KEY_ID'] = output['Credentials']['AccessKeyId']
             os.environ['AWS_SECRET_ACCESS_KEY'] = output['Credentials']['SecretAccessKey']
             os.environ['AWS_SESSION_TOKEN'] = output['Credentials']['SessionToken']
+            print(os.getcwd())
--- jbcli/jbcli/utils/dockerutil.py
+++ jbcli/jbcli/utils/dockerutil.py
@@ -70,7 +70,6 @@ class WatchHandler(FileSystemEventHandler):
 def _intersperse(el, l):
     return [y for x in zip([el]*len(l), l) for y in x]
 
-
 def docker_compose(args, env=None, ganesha=False):
     # Since our docker-compose.yml file is the first one we pass,
     # we need to pass `--project-name` and `--project-directory`.
@@ -80,6 +79,8 @@ def docker_compose(args, env=None, ganesha=False):
         compose_files.append('docker-compose.ganesha.yml')
     file_args = _intersperse('-f', compose_files)
     env_name = os.path.basename(os.path.abspath('.'))
+    print(os.path.abspath('.'))
+    print(env_name)
     cmd = [
         'docker-compose', '--project-directory', '.',
         '--project-name', env_name
@@ -88,7 +89,7 @@ def docker_compose(args, env=None, ganesha=False):
 
 
 def up(env=None, ganesha=False):
-    """Starts and optionally creates a Docker environment based on
+    """Starts and optionally creates        a Docker environment based on
     docker-compose.yml """
     docker_compose(['up', '--abort-on-container-exit'], env=env, ganesha=ganesha)
 
