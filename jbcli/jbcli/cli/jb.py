f3bab9e7b9ffba0d14a480a4e8cf9f12a7583f97
Casey Wireman <cwire4@gmail.com>
2021-09-17 13:03:02 -0400

    add hstm activate back but remove the part where it sets the credentials.  this inadvertently got rid of the powerbar

--- jbcli/jbcli/cli/jb.py
+++ jbcli/jbcli/cli/jb.py
@@ -447,6 +447,9 @@ def start(ctx, env, noupdate, noupgrade, ssh, ganesha, hstm, core, dev_recipe):
 
     if not noupdate:
         dockerutil.pull(tag=tag)
+    if is_hstm:
+        activate_hstm()
+        print("Activating HSTM")
     cleanup_ssh(env)
     if ssh:
         environ.update(activate_ssh(env, environ))
@@ -613,3 +616,8 @@ def dc(args, env, ganesha):
         env = dockerutil.check_home()
     os.chdir(os.path.join(DEVLANDIA_DIR, 'environments', env))
     dockerutil.docker_compose(cmd, ganesha=ganesha)
+
+
+def activate_hstm():
+    with open(".env", "a") as env_dot:
+        env_dot.write(f"\nJB_HSTM=on")
